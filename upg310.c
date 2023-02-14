97463c3635259a1856461f29884292e62c1f73e6
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2023-02-14 11:44:46 +0100

    Create upg310.c

---
+++ upg310.c
@@ -0,0 +1,104 @@
+/*
+ * 	startup.c
+ *
+ */
+__attribute__((naked)) __attribute__((section (".start_section")) )
+void startup ( void )
+{
+__asm__ volatile(" LDR R0,=0x2001C000\n");		/* set stack */
+__asm__ volatile(" MOV SP,R0\n");
+__asm__ volatile(" BL main\n");					/* call main */
+__asm__ volatile(".L1: B .L1\n");				/* never return */
+}
+
+#define STK_CTRL ((volatile unsigned int *)(0xE000E010))  
+#define STK_LOAD ((volatile unsigned int *)(0xE000E014))  
+#define STK_VAL ((volatile unsigned int *)(0xE000E018))  
+#define Bargraph ((volatile unsigned int *)(0x40021014))
+#define GPIO_MODER ((volatile unsigned int *)(0x40021000))
+
+__attribute__((naked))
+void graphic_initalize(void)
+{
+	__asm volatile (".HWORD 0xDFF0\n");
+	__asm volatile (" BX LR\n");
+}
+
+__attribute__((naked))
+void graphic_clear_screen(void)
+{
+	__asm volatile (".HWORD 0xDFF1\n");
+	__asm volatile (" BX LR\n");
+}
+
+__attribute__((naked))
+void graphic_pixel_set(int x, int y)
+{
+	__asm volatile (".HWORD 0xDFF2\n");
+	__asm volatile (" BX LR\n");
+}
+
+__attribute__((naked))
+void graphic_pixel_clear(int x, int y)
+{
+	__asm volatile (".HWORD 0xDFF3\n");
+	__asm volatile (" BX LR\n");
+}
+
+void delay_250ns( void )
+{
+/* SystemCoreClock = 168000000 */
+*STK_CTRL = 0;
+*STK_LOAD = ( (168/4) -1 );
+*STK_VAL = 0;
+*STK_CTRL = 5;
+while( (*STK_CTRL & 0x10000 )== 0 );
+*STK_CTRL = 0;
+}
+
+void delay_micro(unsigned int us)
+{
+while( us > 0 )
+{
+delay_250ns();
+delay_250ns();
+delay_250ns();
+delay_250ns();
+us--;
+}
+}
+
+void delay_milli(unsigned int ms)
+{
+	while(ms>0){
+		int i = 50;
+		while(i>0){
+			delay_250ns();
+			i = i-1;
+		}
+		ms--;
+	}
+}
+
+void init_app(void)
+{
+	* ((unsigned long *) 0x40021000) = 0x5555;
+
+}
+
+void main(void)
+{
+	int i;
+	graphic_initalize();
+	graphic_clear_screen();
+	for(i = 1; i <= 128; i++)
+		graphic_pixel_set(i, 10);
+	for(i = 1; i <= 64; i++)
+		graphic_pixel_set(10, i);
+	delay_milli(500);
+	for(i = 1; i <= 128; i++)
+		graphic_pixel_clear(i, 10);
+	for(i = 1; i <= 64; i++)
+		graphic_pixel_clear(10, i);
+}
+
