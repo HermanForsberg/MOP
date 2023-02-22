94e4edf1fbc4bfcacefc1a747bbccbdaa4e30532
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2023-02-22 13:13:49 +0100

    Update upg403.c

--- upg403.c
+++ upg403.c
@@ -11,71 +11,102 @@ __asm__ volatile(" BL main\n");					/* call main */
 __asm__ volatile(".L1: B .L1\n");				/* never return */
 }
 
+
 #define STK_CTRL ((volatile unsigned int *)(0xE000E010))  
 #define STK_LOAD ((volatile unsigned int *)(0xE000E014))  
 #define STK_VAL ((volatile unsigned int *)(0xE000E018))  
-#define Bargraph ((volatile unsigned int *)(0x40021014))
+
 
 #define B_E 0x40 /* Enable-signal */
 #define B_SELECT 4 /* Välj ASCII-display */
 #define B_RW 2 /* 0=Write, 1=Read */
 #define B_RS 1 /* 0=Control, 1=Data */
 
+
+#define SBC_VTOR ((volatile unsigned int*) (0xE000ED08))
+
 #define GPIO_E_MODER ((volatile unsigned int *)(0x40021000))
-
-#define SBC_VTOR (volatile unsigned int*) 0xE000ED08
-
 #define GPIO_E_IDRLOW ((volatile unsigned char *)(0x40021010))
 #define GPIO_E_IDRHIGH ((volatile unsigned char *)(0x40021011))
-
 #define GPIO_E_ODRLOW ((volatile unsigned char *)(0x40021014))
 #define GPIO_E_ODRHIGH ((volatile unsigned char *)(0x40021015))
 
 
-void delay_250ns( void )
+#define GPIO_D_MODER ((volatile unsigned int *)(0x40020C00))
+#define GPIO_D_IDRLOW ((volatile unsigned char *)(0x40020C10))
+#define GPIO_D_IDRHIGH ((volatile unsigned char *)(0x40020C11))
+#define GPIO_D_ODRLOW ((volatile unsigned char *)(0x40020C14))
+#define GPIO_D_ODRHIGH ((volatile unsigned char *)(0x40020C15))
+
+static int systick_flag;
+
+//DELAY==============================================================================
+
+
+void delay_1mikro(void)
 {
-/* SystemCoreClock = 168000000 */
-*STK_CTRL = 0;
-*STK_LOAD = ( (168/4) -1 );
-*STK_VAL = 0;
-*STK_CTRL = 5;
-while( (*STK_CTRL & 0x10000 )== 0 );
-*STK_CTRL = 0;
+	systick_flag = 0;
+	*STK_CTRL = 0;
+	*STK_LOAD = ( 168 - 1 );
+	*STK_VAL = 0;
+	*STK_CTRL = 7;
 }
 
-void delay_micro(unsigned int us)
+unsigned int delay_count;
+void delay(unsigned int count)
 {
-while( us > 0 )
-{
-delay_250ns();
-delay_250ns();
-delay_250ns();
-delay_250ns();
-us--;
-}
+	if (count == 0) return;
+	delay_count = count;
+	delay_1mikro();
 }
 
 
+//HANDLER==============================================================================
+void systick_irq_handler(void)
+{
+	*STK_CTRL = 0;
+	delay_count -= 1;
+	if(delay_count > 0)
+	{
+		delay_1mikro();
+	}else{
+		systick_flag = 1;
+	}
+}
 
-#ifdef SIMULATOR
-#define DELAY_COUNT 100
-#else
-#define DELAY_COUNT 1000000
-#endif
+
+//INIT==============================================================================
+
+void init_handler_systick(void)
+{
+	*((void (**)(void)) 0x2001C03C) = systick_irq_handler;
+	systick_flag = 0;
+}
+
+
+
+#define DELAY_COUNT 1000
+
 void init_app(void)
 {
-	
+	*SBC_VTOR = 0x2001C000;
+	*GPIO_D_MODER = 0x00005555;
+	init_handler_systick();
 }
 
 void main(void)
 {
 	init_app();
-	*GPIO_ODR_LOW = 0;
+	*GPIO_D_ODRLOW = 0;
 	delay(DELAY_COUNT);
-	*GPIO_ODR_LOW = 0xFF;
+	*GPIO_D_ODRLOW = 0xFF;
 	while(1)
 	{
-		
+		if(systick_flag)
+		{
+			break;
+		}
 	}
+	*GPIO_D_ODRLOW = 0;
 }
 
